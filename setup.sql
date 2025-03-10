6f969196fe7539519af58eb8a9a8c4f00b2aca7a
cookepoli <a.cookepoli@gmail.com>
2025-03-10 09:02:19 -0400

    added reservation and attendees tables

--- setup.sql
+++ setup.sql
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS member (
     email       VARCHAR(64)     NOT NULL,
     phonenum    VARCHAR(12)     NOT NULL,
     optIN       BOOLEAN         NOT NULL,
+    active      BOOLEAN         NOT NULL,
 
     PRIMARY KEY(member_id)
 );
@@ -24,4 +25,28 @@ CREATE TABLE IF NOT EXISTS charges (
 
     FOREIGN KEY(member_id) REFERENCES member(member_ID)
                                    ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS reservation (
+    reservation_id  INT             NOT NULL,
+    court_num       INT             NOT NULL,
+    res_day         INT             NOT NULL,
+    start_time      TIME            NOT NULL,
+    end_time        TIME            NOT NULL,
+    member_ID       INT             NOT NULL,
+    type            VARCHAR(7)      NOT NULL     
+
+    PRIMARY KEY(reservation_id)
+    FOREIGN KEY(member_id) REFERENCES member(member_id)
+                                    ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS attendees (
+    reservation_ID  INT             NOT NULL,
+    firstname       VARCHAR(32)     NOT NULL,
+    lastname        VARCHAR(32)     NOT NULL,
+    member_id       INT,
+
+    FOREIGN KEY(reservation_id) REFERENCES reservation(reservation_id)
+                                    ON DELETE CASCADE
 )
