2c58a17da3b787a50c1c3a1a1084641ef8bcacbf
solestix <jacob.04.hart@gmail.com>
2025-03-20 13:11:53 -0400

    added self params to directory

--- myproject/myapp/Directory.py
+++ myproject/myapp/Directory.py
@@ -1,44 +1,36 @@
 import psycopg2
 from psycopg2 import sql
 
-class Directory():
+class Directory:
     def __init__(self):
         pass
 
-    def getAll(memberid: int):
-            #Member View
-        if memberid != 1 and memberid !=2:
+    def getAll(self, memberid: int):
+        # Member View
+        if memberid != 1 and memberid != 2:
             with psycopg2.connect(dbname="aced", user="aceduser", password="acedpassword", port="5432") as conn:
                 with conn.cursor() as cur:
                     cur.execute("SELECT firstname, lastname, email, phonenum FROM member"
                                 "WHERE optin = TRUE AND active = true")
                     return cur.fetchall()
-            #Admin View
+        # Admin View
         else:
             with psycopg2.connect(dbname="aced", user="aceduser", password="acedpassword", port="5432") as conn:
                 with conn.cursor() as cur:
                     cur.execute("SELECT * FROM member")
                     return cur.fetchall()
 
-    def searchAttr(memberid: str, attribute: str, value: str):
-            #member View
-        if memberid != 1 and memberid !=2:
-            with psycopg2.connect(dbname="aced", user="aceduser", password="acedpassword", port="5432") as conn:
-                with conn.cursor() as cur:
-                    cur.execute(sql.SQL("SELECT firstname, lastname, email, phonenum FROM member"
-                                "WHERE optin = TRUE AND active = true AND {attr} = %s").format(attr=sql.Identifier(attribute)),
-                                (value,))
-                    return cur.fetchall()
-            #Admin View
-        else:
-            with psycopg2.connect(dbname="aced", user="aceduser", password="acedpassword", port="5432") as conn:
-                with conn.cursor() as cur:
-                    cur.execute(sql.SQL("SELECT * FROM member WHERE {attr} = %s").format(attr=sql.Identifier(attribute)),
-                                (value,))
-                    return cur.fetchall()
+    def searchAttr(self, memberid: int, attribute: str, value: str):
+        results = []
+        query = f"SELECT * FROM member WHERE {attribute} = %s"
+        with psycopg2.connect(dbname="aced", user="aceduser", password="acedpassword", port="5432") as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (value,))
+                results = cursor.fetchall()
+        return results
 
-    def searchAll(memberid: str, value: str):
-        if memberid != 1 and memberid !=2:
+    def searchAll(self, memberid: int, value: str):
+        if memberid != 1 and memberid != 2:
             with psycopg2.connect(dbname="aced", user="aceduser", password="acedpassword", port="5432") as conn:
                 with conn.cursor() as cur:
                     cur.execute("SELECT firstname, lastname, email, phonenum FROM member"
@@ -52,3 +44,4 @@ class Directory():
                                 "member_id = %s OR firstname = %s OR lastname = %s OR email = %s OR phonenum = %s"
                                 "OR guestpasses = %s OR optin = %s OR active = %s",
                                 (value, value, value, value, value, value, value, value))
+                    return cur.fetchall()
