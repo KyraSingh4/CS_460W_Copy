7e74e68193f1e05548182231d091a07888bdf5a2
cookepoli <a.cookepoli@gmail.com>
2025-03-24 08:43:38 -0400

    small database changes

--- myproject/myapp/memberTest.py
+++ myproject/myapp/memberTest.py
@@ -1,12 +1,7 @@
-from Member import Member
+from Member import President
 import psycopg2
 from psycopg2 import sql
 
+p = President()
 
-search = "email"
-value = "test@test.com"
-
-with psycopg2.connect(dbname="aced", user="aceduser", password="acedpassword", port="5432") as conn:
-    with conn.cursor() as cur:
-        cur.execute(sql.SQL("UPDATE member SET active = FALSE WHERE {sear} = %s").format(sear=sql.Identifier(search)),
-                    (value,))
+p.createMember("test", "test", "test@test.com", "111-111-1113", True, 'test')
