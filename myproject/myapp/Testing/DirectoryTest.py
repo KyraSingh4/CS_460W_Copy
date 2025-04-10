bfbdc23c815993f6fee8ca2669ffd01eb25ef374
Kyra Singh <kyra.singh4444@gmail.com>
2025-04-10 15:02:24 -0400

    Add testing methods for directory

---
+++ myproject/myapp/Testing/DirectoryTest.py
@@ -0,0 +1,16 @@
+import unittest
+from ..Directory import Directory
+
+class testDirectory(unittest.Testcase):
+    def setup(self):
+        self.directory = Directory()
+    def test_getAll_pos(self):
+        pass
+    def test_searchAttr_pos(self):
+        pass
+    def test_searchAttr_invalid(self):
+        pass
+    def test_nameLookup_pos(self):
+        pass
+    def test_getEmalis_pos(self):
+        pass
