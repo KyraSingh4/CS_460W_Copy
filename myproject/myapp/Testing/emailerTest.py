b18d8fc61adf4705ae15d961de06b9df6f016054
solestix <jacob.04.hart@gmail.com>
2025-04-10 14:19:17 -0400

    emailer unittest

--- myproject/myapp/Testing/emailerTest.py
+++ myproject/myapp/Testing/emailerTest.py
@@ -1,8 +1,18 @@
 import unittest
-from unittest.mock import patch, MagicMock
 from myapp.emailer import Emailer
 
 class TestEmailer(unittest.TestCase):
 
     def setUp(self):
         self.emailer = Emailer()
+
+    def test_send_email_pos(self):
+        self.emailer.connect()
+        self.emailer.sendEmail("Hello World", "Test", "jhart@hartford.edu")
+
+    def test_send_email_neg(self):
+        with self.assertRaises(Exception):
+            self.emailer.sendEmail("Hello World", "Test", "invalid_email")
+
+if __name__ == '__main__':
+    unittest.main()  # Run the tests
