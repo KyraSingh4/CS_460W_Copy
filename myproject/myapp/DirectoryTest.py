9f350ef146b0ff844982d736f1900e1d9e036729
Kyra Singh <kyra.singh4444@gmail.com>
2025-04-12 01:54:46 -0400

    Completed searchAttr testing and nameLookup positive testing

---
+++ myproject/myapp/DirectoryTest.py
@@ -0,0 +1,29 @@
+import unittest
+import Directory
+
+class testDirectory(unittest.TestCase):
+    def setUp(self):
+        self.directory = Directory.Directory()
+
+    # def test_getAll(self):
+    #     # self.assertEqual(self.directory.getAll(), )
+    #     pass
+
+    def test_searchAttr_pos(self):
+        self.assertEqual(self.directory.searchAttr(4, 'phonenum', '555-555-5555'), [('Shalin', 'Singh', 'kyrals476@gmail.com', '555-555-5555')], "error")
+
+    def test_searchAttr_invalid(self):
+        self.assertFalse(self.directory.searchAttr(4, 'email', 'invalidinput'))
+
+    def test_nameLookup_pos(self):
+        self.assertEqual(self.directory.nameLookup('Kyra', 'Singh'), 5, "error")
+
+    def test_nameLookup_negative(self):
+        # self.assertFalse(self.directory.nameLookup('', ''))
+        pass
+
+    # def test_getEmails_pos(self):
+    #     pass
+
+if __name__ == '__main__':
+    unittest.main()
