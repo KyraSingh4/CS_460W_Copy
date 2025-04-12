c8085f17f27887f2c7150872bb78be09aee6be89
KyraSingh4 <94869006+KyraSingh4@users.noreply.github.com>
2025-04-12 18:13:11 -0400

    uncomment unit test

--- myproject/myapp/DirectoryTest.py
+++ myproject/myapp/DirectoryTest.py
@@ -5,18 +5,14 @@ class testDirectory(unittest.TestCase):
     def setUp(self):
         self.directory = Directory.Directory()
 
-    # def test_getAll(self):
-    #     # self.assertEqual(self.directory.getAll(), )
-    #     pass
-
     def test_searchAttr_pos(self):
         self.assertEqual(self.directory.searchAttr(4, 'phonenum', '555-555-5555'), [('Shalin', 'Singh', 'kyrals476@gmail.com', '555-555-5555')], "error")
 
     def test_searchAttr_invalid(self):
         self.assertFalse(self.directory.searchAttr(4, 'email', 'invalidinput'))
 
-    # def test_searchAttr_invalidattr(self):
-    #     self.assertFalse(self.directory.searchAttr(4, 'invalid', 'Kyra'))
+    def test_searchAttr_invalidattr(self):
+        self.assertFalse(self.directory.searchAttr(4, 'invalid', 'Kyra'))
 
     def test_nameLookup_pos(self):
         self.assertEqual(self.directory.nameLookup('Kyra', 'Singh'), 5, "error")
